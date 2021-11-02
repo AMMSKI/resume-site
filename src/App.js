c2ed5dd0639ffe02243ba640d967710dc3bfc4f7
Alan Mamulski <alanmamulski@gmail.com>
2021-11-02 11:38:07 -0600

    First two sections skeleton setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Hero from './Components/Hero';
+import image from './Images/editedBackground.png'
+import styled from 'styled-components';
+import Navigation from './Components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer className='App'>
       <div>
-      <Hero />
+        <Navigation />
       </div>
-    </div>
+      <div className='hero_container'>
+        <Hero />
+      </div>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  display: 'flex';
+`
+
 
 export default App;
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,10 +2,22 @@ import React from "react";
 
 const Hero = () => {
   return(
-    <div>
-      Hero
+    <div style={styles.heroDiv}>
+      <div>
+        <h1>Alan Mamulski</h1>
+        <h3>Web Developer</h3>
+      </div>
     </div>
   )
+}
+
+const styles = {
+  heroDiv:{
+    borderColor: 'black',
+    border: '2px solid',
+    height: '90vh',
+    padding: '10px',
+  }
 }
 
 export default Hero
