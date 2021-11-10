b3578a530360984214e6bff740cfd76c09a76163
Alan Mamulski <alanmamulski@gmail.com>
2021-11-10 11:41:24 -0700

    changed about content

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -9,7 +9,7 @@ const AboutMe = () => {
       </div>
       <div className='about_body'>
         <p> 
-          I’m a passionate software developer who enjoys working in both the frontend and the backend.  I’m a big fan of JavaScript and believe just about anything can be written in ES6. Software has given me the opportunity to love what I do everyday. If that’s not the most important thing in life, I don’t know what is.  If I’m not coding I’m probably golfing or enjoying the outdoors with my wife and two beautiful daughters. 
+          I’m a passionate software developer who enjoys working in both the frontend and the backend. I took a big risk leaving my sales career but I couldn't be happier. Software gives me the opportunity to love what I do everyday. If I’m not coding I’m probably golfing or enjoying the outdoors with my wife and two beautiful daughters. 
         </p>
       </div>
         <a href='https://www.canva.com/design/DAEtxdDd0_w/2a-jMvSZtWEsNCbN80xxkg/view?utm_content=DAEtxdDd0_w&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton' target='_blank' rel="noreferrer">View Resume</a>
